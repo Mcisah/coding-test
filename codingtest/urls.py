da2738948b42b1c865ea55bb241ca330f3bf8d2e
Michael Isah <michaelisahadav@gmail.com>
2019-08-27 13:35:42 +0100

    fix: static ish

--- codingtest/urls.py
+++ codingtest/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
